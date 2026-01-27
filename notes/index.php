589776d0c384ef5decb027bd657d8dd5831bdc5b
pocket-98 <dayalpavan@gmail.com>
2026-01-27 00:43:02 -0700

    initial setup

---
+++ notes/index.php
@@ -0,0 +1,13 @@
+<pre>
+## infrastructure
+* proxmox hypervisor on a cloud provider vps
+* opnsense firewall/router vm on server as gateway
+* archlinux vm on server with apache and php and sqlite3
+* cloudflare zerotrust tls tunnel for ddos and to reverse proxy to provide ssl
+
+## theme
+* raw html and css (no js bc stallman would not approve)
+* inspired by geocities 90's themed websites
+* bootstrap modern css layout with <a href="https://code.divshot.com/geo-bootstrap/">this awesome theme</a>
+* php to provide async basic logging of number of visitors to sqlite file to provide website counter
+</pre>
